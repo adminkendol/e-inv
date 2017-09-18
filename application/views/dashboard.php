78233d41f2312e05fe00eda894be21e78c0861fc
chandra <chandra.irawan@immobisp.com>
2017-09-18 07:34:04 +0700

    fix Dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -6,15 +6,15 @@
                     <i class="material-icons">content_copy</i>
                 </div>
                 <div class="card-content">
-                    <p class="category">Used Space</p>
-                    <h3 class="title">49/50
-                        <small>GB</small>
+                    <p class="category">Stok Kadaluarsa</p>
+                    <h3 class="title">9/50
+                        <small>Item</small>
                     </h3>
                 </div>
                 <div class="card-footer">
                     <div class="stats">
                         <i class="material-icons text-danger">warning</i>
-                        <a href="#pablo">Get More Space...</a>
+                        <a href="#">Check stok...</a>
                     </div>
                 </div>
             </div>
@@ -25,12 +25,12 @@
                     <i class="material-icons">store</i>
                 </div>
                 <div class="card-content">
-                    <p class="category">Revenue</p>
-                    <h3 class="title">$34,245</h3>
+                    <p class="category">Keuntungan</p>
+                    <h3 class="title">RP 34.245.000</h3>
                 </div>
                 <div class="card-footer">
                     <div class="stats">
-                        <i class="material-icons">date_range</i> Last 24 Hours
+                        <i class="material-icons">date_range</i> Last 1 Mounth
                     </div>
                 </div>
             </div>
@@ -51,23 +51,6 @@
                 </div>
             </div>
         </div>
-        
-                        <!--<div class="col-lg-3 col-md-6 col-sm-6">
-                            <div class="card card-stats">
-                                <div class="card-header" data-background-color="blue">
-                                    <i class="fa fa-twitter"></i>
-                                </div>
-                                <div class="card-content">
-                                    <p class="category">Followers</p>
-                                    <h3 class="title">+245</h3>
-                                </div>
-                                <div class="card-footer">
-                                    <div class="stats">
-                                        <i class="material-icons">update</i> Just Updated
-                                    </div>
-                                </div>
-                            </div>
-                        </div>-->
     </div>
     <div class="row">
         <div class="col-md-6">
@@ -121,5 +104,4 @@
             </div>
         </div>
     </div>
-                    
 </div>
